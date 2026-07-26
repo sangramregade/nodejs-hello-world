7d2d922b7efff4432ac58bfa8dc454c316026a10
sregade <sangram.regade@smartedge.in>
2026-07-27 00:12:47 +0530

    cc

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(3000, () => {
+serverff.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
