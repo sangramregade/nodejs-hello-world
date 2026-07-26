96bdd58d13abd56ae46f4c26314bc9aec1e1b97c
Sangram Regade <58468112+sangramregade@users.noreply.github.com>
2026-07-27 00:14:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-serverff.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
